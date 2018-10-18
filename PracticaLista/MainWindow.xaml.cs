5023453ce7fefa1f2db43ae76402ab3eaaea364b
german adrian anguiano perez <adrian2009@live.com.mx>
2018-10-18 08:48:40 -0700

    mejorado

--- PracticaLista/MainWindow.xaml.cs
+++ PracticaLista/MainWindow.xaml.cs
@@ -24,11 +24,28 @@ namespace PracticaLista
         public MainWindow()
         {
             InitializeComponent();
+            Materia historia = new Materia("HST123", "Historia");
+            Materia matematicas = new Materia("MAT456", "Matemáticas");
+            Materia civismo = new Materia("CIV741", "Civismo");
+            Materia Español = new Materia("ESP123", "Español");
+            Materia Artistica = new Materia("ART512", "Artistica");
+
             alumnos.Add(new Alumno("Juan Perez","179165","Lic. en Derecho"));
             alumnos.Add(new Alumno("Pedro Lopez", "176134", "Lic. en Psicologia"));
             alumnos.Add(new Alumno("Maria Garcia", "186154", "Lic. en Finanzas"));
             alumnos.Add(new Alumno("Ana Valenzuela", "145128", "Ing. Civil"));
 
+            //primer parcial
+            alumnos[0].Materias.Add(Español);
+            alumnos[0].Materias.Add(Artistica);
+            //segundo
+            alumnos[1].Materias.Add(civismo);
+            alumnos[1].Materias.Add(matematicas);
+            //tercero
+            alumnos[2].Materias.Add(historia);
+            alumnos[2].Materias.Add(civismo);
+            //final
+            alumnos[3].Materias.Add(matematicas);
             foreach(Alumno alumno in alumnos)
             {
                 lstAlumnos.Items.Add(
@@ -45,6 +62,26 @@ namespace PracticaLista
             lblNombre.Text = alumnos[lstAlumnos.SelectedIndex].Nombre;
             lblMatricula.Text = alumnos[lstAlumnos.SelectedIndex].Matricula;
             lblCarrera.Text = alumnos[lstAlumnos.SelectedIndex].Carrera;
+
+            lstMaterias.Items.Clear();
+
+            foreach(Materia materia in alumnos[lstAlumnos.SelectedIndex].Materias)
+            {
+                lstMaterias.Items.Add(new ListBoxItem()
+                {
+                    Content = materia.Nombre
+                });
+                };
+            }
+
+        private void lstMaterias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstMaterias.SelectedIndex !=-1) {
+                lbnNombreMateria.Text = alumnos[lstAlumnos.SelectedIndex].Materias[lstMaterias.SelectedIndex].Nombre;
+                lblclavemateria.Text = alumnos[lstAlumnos.SelectedIndex].Materias[lstMaterias.SelectedIndex].Clave;
+            }
         }
     }
-}
+    }
+
+
